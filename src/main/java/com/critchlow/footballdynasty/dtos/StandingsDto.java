889fb895918d028b88583f2d04e4fa73025d2b51
Darin Critchlow <darincritchlow@gmail.com>
2024-06-19 21:50:22 -0600

    Add mapper. Add MvcConfigurer

---
+++ src/main/java/com/critchlow/footballdynasty/dtos/StandingsDto.java
@@ -0,0 +1,15 @@
+package com.critchlow.footballdynasty.dtos;
+
+import com.critchlow.footballdynasty.model.Team;
+
+import java.util.UUID;
+
+public class StandingsDto {
+    public UUID id;
+    public Team team;
+    public int year;
+    public int wins;
+    public int losses;
+    public Integer rank;
+    public Integer receiving_votes;
+}
