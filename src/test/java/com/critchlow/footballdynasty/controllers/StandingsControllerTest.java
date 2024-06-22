768af21792e8fe7c025018169efb3da62890533a
Darin Critchlow <darincritchlow@gmail.com>
2024-06-22 17:21:02 -0600

    Add TestContainers for postgres

--- src/test/java/com/critchlow/footballdynasty/controllers/StandingsControllerTest.java
+++ src/test/java/com/critchlow/footballdynasty/controllers/StandingsControllerTest.java
@@ -5,13 +5,19 @@ import com.critchlow.footballdynasty.model.Standings;
 import com.critchlow.footballdynasty.model.Team;
 import com.critchlow.footballdynasty.repository.StandingsRepository;
 import com.critchlow.footballdynasty.services.StandingsService;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mock;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
 import org.springframework.test.web.servlet.MockMvc;
+import org.testcontainers.containers.PostgreSQLContainer;
 
 import java.util.List;
 import java.util.UUID;
@@ -23,7 +29,7 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-@SpringBootTest
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @AutoConfigureMockMvc
 class StandingsControllerTest {
     @Autowired
@@ -34,6 +40,30 @@ class StandingsControllerTest {
 
     @MockBean
     StandingsRepository standingsRepository;
+
+    @LocalServerPort
+    private Integer port;
+
+    static PostgreSQLContainer<?> postgres = new PostgreSQLContainer<>(
+            "postgres:16-alpine"
+    );
+
+    @BeforeAll
+    static void beforeAll() {
+        postgres.start();
+    }
+
+    @AfterAll
+    static void afterAll() {
+        postgres.stop();
+    }
+
+    @DynamicPropertySource
+    static void configureProperties(DynamicPropertyRegistry registry) {
+        registry.add("spring.datasource.url", postgres::getJdbcUrl);
+        registry.add("spring.datasource.username", postgres::getUsername);
+        registry.add("spring.datasource.password", postgres::getPassword);
+    }
 
     
     @Test
