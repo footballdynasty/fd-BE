5d1f5f905d8e2a87205d7f62ede1320438b2c5f1
Tyler Critchlow <tylerdarincritchlow@gmail.com>
2024-07-30 22:21:51 -0600

    added test db connection

--- src/test/java/com/critchlow/footballdynasty/controllers/ScheduleControllerTest.java
+++ src/test/java/com/critchlow/footballdynasty/controllers/ScheduleControllerTest.java
@@ -5,13 +5,18 @@ import com.critchlow.footballdynasty.model.Schedule;
 import com.critchlow.footballdynasty.model.Team;
 import com.critchlow.footballdynasty.repository.GameRepository;
 import com.critchlow.footballdynasty.services.ScheduleService;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mock;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
 import org.springframework.test.web.servlet.MockMvc;
+import org.testcontainers.containers.PostgreSQLContainer;
 
 import java.sql.Date;
 import java.util.List;
@@ -33,6 +38,28 @@ public class ScheduleControllerTest {
 
     @MockBean
     GameRepository gameRepository;
+
+    static PostgreSQLContainer<?> postgres = new PostgreSQLContainer<>(
+            "postgres:16-alpine"
+    );
+
+    @BeforeAll
+    static void beforeAll() {
+        postgres.start();
+    }
+
+    @AfterAll
+    static void afterAll() {
+        postgres.stop();
+    }
+
+    @DynamicPropertySource
+    static void configureProperties(DynamicPropertyRegistry registry) {
+        registry.add("spring.datasource.url", postgres::getJdbcUrl);
+        registry.add("spring.datasource.username", postgres::getUsername);
+        registry.add("spring.datasource.password", postgres::getPassword);
+    }
+
 
     @Test
     public void getSchedule_thenResultReturned() throws Exception {
