8babe6a4c411d00282edcd45ce3da31619843d5e
Darin Critchlow <darincritchlow@gmail.com>
2024-08-02 21:45:54 -0600

    Update Game compare to use Home team name

--- src/main/java/com/critchlow/footballdynasty/model/GameComparator.java
+++ src/main/java/com/critchlow/footballdynasty/model/GameComparator.java
@@ -5,12 +5,6 @@ import java.util.Comparator;
 public class GameComparator implements Comparator<Game> {
     @Override
     public int compare(Game g1, Game g2) {
-        if(g1.homeTeam.username == null){
-            return -1;
-        }
-        if(g2.homeTeam.username == null){
-            return 1;
-        }
-        return g1.homeTeam.username.compareTo(g2.homeTeam.username);
+        return g1.homeTeam.name.compareTo(g2.homeTeam.name);
     }
 }
